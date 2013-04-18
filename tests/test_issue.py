4c565d1bba0845c831bf011a5fd0a25644ab3f0d
Stanislav Vitko <vist.rcm@gmail.com>
2013-04-18 18:01:17 +0400

    initial tests commited

---
+++ tests/test_issue.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+__author__ = 'vist'
+
+
+class TestIssue(TestCase):
+    def test_field(self):
+        self.fail()
---
+++ tests/test_jira.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+__author__ = 'vist'
+
+
+class TestJira(TestCase):
+    def test_search_issues(self):
+        self.fail()
+
+    def test_issue(self):
+        self.fail()
